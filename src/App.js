b7cf534c432e1d99ed6c7d9efa4349b3911a7aa4
syedmoeen <syedmuhammadmoin@gmail.com>
2020-09-26 20:21:37 +0500

    New react project GITHUB FINDER

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;
